699aa39d81d4eba3a8a5136fe1f114d644f8e415
luyanbo <robert.lyb@alibaba-inc.com>
2022-04-21 11:56:08 +0800

    init

---
+++ main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"fmt"
+	beego "github.com/beego/beego/v2/server/web"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/clients/naming_client"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+	_ "github.com/robberphex/beego-nacos-example/routers"
+	"go.uber.org/multierr"
+	"net"
+	"os"
+	"time"
+)
+
+const APP_NAME = "test_app"
+
+var ignoreNets = []string{
+	"30.39.179.16/30",
+	"fe80::1/24",
+}
+var allowNets = []string{}
+
+type callback struct {
+	ips     []string
+	nclient naming_client.INamingClient
+}
+
+func newCallback() callback {
+	ip := getRegIp()
+	clientConfig := *constant.NewClientConfig(
+		constant.WithNamespaceId(""), //When namespace is public, fill in the blank string here.
+		constant.WithTimeoutMs(5000),
+		constant.WithNotLoadCacheAtStart(true),
+		constant.WithLogDir("/tmp/nacos/log"),
+		constant.WithCacheDir("/tmp/nacos/cache"),
+		constant.WithLogLevel("debug"),
+	)
+	serverConfigs := []constant.ServerConfig{
+		*constant.NewServerConfig(
+			"mse-96efa264-p.nacos-ans.mse.aliyuncs.com",
+			8848,
+			constant.WithScheme("http"),
+		),
+	}
+
+	// Create naming client for service discovery
+	nClient, _ := clients.CreateNamingClient(map[string]interface{}{
+		"serverConfigs": serverConfigs,
+		"clientConfig":  clientConfig,
+	})
+	return callback{
+		ips:     ip,
+		nclient: nClient,
+	}
+}
+
+func (c callback) AfterStart(app *beego.HttpServer) {
+	fmt.Printf("current PID: %d, ips: %+v\n", os.Getpid(), c.ips)
+
+	var totalErr error
+	for _, ip := range c.ips {
+		regParam := vo.RegisterInstanceParam{
+			Ip:          ip,
+			Port:        uint64(app.Cfg.Listen.HTTPPort),
+			Weight:      100,
+			Enable:      true,
+			Healthy:     true,
+			ServiceName: APP_NAME,
+			Ephemeral:   true,
+			//Metadata: map[string]string{
+			//	"preserved.heart.beat.interval": "3000",
+			//},
+		}
+		_, err := c.nclient.RegisterInstance(regParam)
+		totalErr = multierr.Append(totalErr, err)
+	}
+	fmt.Printf("%#v\n", totalErr)
+}
+
+func (c callback) BeforeShutdown(app *beego.HttpServer) {
+	var totalErr error
+	for _, ip := range c.ips {
+		updateParam := vo.UpdateInstanceParam{
+			Ip:          ip,
+			Port:        uint64(app.Cfg.Listen.HTTPPort),
+			Weight:      0,
+			Enable:      false,
+			ServiceName: APP_NAME,
+		}
+		_, err := c.nclient.UpdateInstance(updateParam)
+		totalErr = multierr.Append(totalErr, err)
+	}
+	fmt.Printf("%#v\n", totalErr)
+
+	totalErr = nil
+	for _, ip := range c.ips {
+		deregParam := vo.DeregisterInstanceParam{
+			Ip:          ip,
+			Port:        uint64(app.Cfg.Listen.HTTPPort),
+			ServiceName: APP_NAME,
+		}
+		_, err := c.nclient.DeregisterInstance(deregParam)
+		totalErr = multierr.Append(totalErr, err)
+	}
+	fmt.Printf("%#v\n", totalErr)
+	time.Sleep(10 * time.Second)
+}
+
+func main() {
+	beego.RunWithOptions(nil, beego.WithLifeCycleCallback(newCallback()))
+}
+
+func getRegIp() []string {
+	var ignoreSubnets []*net.IPNet
+	for _, ignoreNet := range ignoreNets {
+		_, ignoreSubnet, _ := net.ParseCIDR(ignoreNet)
+		ignoreSubnets = append(ignoreSubnets, ignoreSubnet)
+	}
+
+	var allowSubnets []*net.IPNet
+	for _, allowNet := range allowNets {
+		_, allowSubnet, _ := net.ParseCIDR(allowNet)
+		allowSubnets = append(allowSubnets, allowSubnet)
+	}
+
+	var res []string
+
+	ifaces, _ := net.Interfaces()
+	// handle err
+	for _, i := range ifaces {
+		addrs, _ := i.Addrs()
+		// handle err
+	addrLoop:
+		for _, addr := range addrs {
+			if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+				for _, ignoreSubnet := range ignoreSubnets {
+					if ignoreSubnet.Contains(ipNet.IP) {
+						continue addrLoop
+					}
+				}
+				if len(allowSubnets) == 0 {
+					res = append(res, ipNet.IP.String())
+				} else {
+					for _, allowSubnet := range allowSubnets {
+						if allowSubnet.Contains(ipNet.IP) {
+							res = append(res, ipNet.IP.String())
+							continue addrLoop
+						}
+					}
+				}
+			}
+		}
+	}
+	return res
+}
---
+++ routers/router.go
@@ -0,0 +1,34 @@
+package routers
+
+import (
+	"fmt"
+	beego "github.com/beego/beego/v2/server/web"
+	beecontext "github.com/beego/beego/v2/server/web/context"
+	"github.com/robberphex/beego-nacos-example/controllers"
+	"net/http"
+	"strings"
+)
+
+func init() {
+	//beego.Router("/", &controllers.MainController{}, "get,post:Get")
+	beego.Router("/", &controllers.MainController{}, "get,post:Get")
+	beego.Router("/a", &controllers.MainController{})
+	beego.Router("/a/bc", &controllers.MainController{})
+	beego.Router("/api/:id([0-9]+)", &controllers.MainController{})
+
+	x := beego.BeeApp.Handlers
+	for i, info := range x.GetAllControllerInfo() {
+		fmt.Printf("%d\t=\t%s\t%#v\n", i, info.GetPattern(), info.GetMethod())
+	}
+
+	bctx := beecontext.NewContext()
+	bctx.Input.Context = beecontext.NewContext()
+	req, err := http.NewRequest("GET", "/", strings.NewReader("x"))
+	_ = err
+	bctx.Input.Context.Request = req
+	r, found := x.FindRouter(bctx)
+	p := r.GetPattern()
+	fmt.Println("pattern:\t" + p)
+	_, _ = r, found
+	_ = x
+}
